e59a00f8b6bb830ce1c9522cdaa80b70d097d7f7
Michael John Rieser <michi.rieser@gmail.com>
2014-10-31 15:31:52 +0100

    Add minimize side function in java script. Set height of page to 100%.

---
+++ js/custom.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {  
+  // minimize side bar
+  $('#sidebar-minimize').on('click', function() {
+    minimizeSideBar();  
+  });
+});
+
+function minimizeSideBar() {
+  // hide sidebar
+  if( $('#page-sidebar').is(':visible') ) {
+    $('#sidebar-minimize span').removeClass('fa-dedent').addClass('fa-indent');
+    
+    $('#page-sidebar').animate({'width': 0}, 400, function() {
+      $('#page-sidebar').hide();
+    });
+    $('#page-content').animate({'left': 0}, 400);
+    
+  // show sidebar
+  } else {
+    $('#sidebar-minimize span').removeClass('fa-indent').addClass('fa-dedent');
+    
+    $('#page-sidebar').show();
+    $('#page-sidebar').animate({'width': '220px'}, 400);
+    $('#page-content').animate({'left': '220px'}, 400);
+  }
+}
