0d63521dbf0630f2302d996595ed1124c47977f4
Michael John Rieser <michi.rieser@gmail.com>
2014-11-04 22:26:07 +0100

    Add JavaScript function for sub navigation

--- js/custom.js
+++ js/custom.js
@@ -3,11 +3,16 @@ $(document).ready(function() {
   $('#sidebar-minimize').on('click', function() {
     minimizeSideBar();  
   });
+  
+  // navigation
+  $('#sidebar-navigation li > a').on('click', function() {
+    subNavi($(this));
+  });
 });
 
 function minimizeSideBar() {
   // hide sidebar
-  if( $('#page-sidebar').is(':visible') ) {
+  if ($('#page-sidebar').is(':visible')) {
     $('#sidebar-minimize span').removeClass('fa-dedent').addClass('fa-indent');
     
     $('#page-sidebar').animate({'width': 0}, 400, function() {
@@ -23,4 +28,16 @@ function minimizeSideBar() {
     $('#page-sidebar').animate({'width': '220px'}, 400);
     $('#page-content').animate({'marginLeft': '220px'}, 400);
   }
+}
+
+function subNavi($this) {
+  var $subNavi = $this.next();
+  
+  if ($subNavi.is(':visible')) {
+    $subNavi.slideUp();
+  } else {
+    $subNavi.slideDown();
+  }
+  
+  return false;
 }
