e3b41240269515c6cbccbcd9a99c25b0463c97a2
Michael John Rieser <michi.rieser@gmail.com>
2014-11-10 11:27:10 +0100

    Save access token in sessionStorage. Add logout function.

--- app/login/login.js
+++ app/login/login.js
@@ -1,7 +1,11 @@
 var login = angular.module('login', []);
 
 login.controller('LoginCtrl', ['$scope', 'loginService', function($scope, loginService) {
-  $scope.login = function(data) {
-    loginService.login(data);
+  $scope.login = function(user) {
+    loginService.login(user);
   };
+
+  $scope.logout = function() {
+    loginService.logout();
+  }
 }]);
--- app/services/login.js
+++ app/services/login.js
@@ -1,14 +1,14 @@
-app.factory('loginService', ['$http', function($http) {
+app.factory('loginService', ['$http', '$location', function($http, $location) {
   return {
-    login: function(data) {
+    login: function(user) {
       $http({
         method: 'POST',
         url: '/request.php',
         data: {
           'url': 'https://uaa.run.pivotal.io/oauth/token',
           'grant_type': 'password',
-          'password': data.password,
-          'username': data.email,
+          'password': user.password,
+          'username': user.email,
           'scope': ''
         },
         headers: {
@@ -17,10 +17,26 @@ app.factory('loginService', ['$http', function($http) {
           'Authorization': 'Basic Y2Y6',
         }
       }).success(function(data, status, headers, config) {
-        console.log(data);
+        if (data.access_token != null) {
+          sessionStorage.setItem('access_token', data.access_token);
+          sessionStorage.setItem('refresh_token', data.refresh_token);
+          sessionStorage.setItem('logged_in', true);
+
+          $location.path('/dashboard');
+        } else {
+          console.log('login failed');
+        }
       }).error(function(data, status, headers, config) {
         console.log(data);
       });
+    },
+
+    logout: function() {
+      sessionStorage.removeItem('access_token');
+      sessionStorage.removeItem('refresh_token');
+      sessionStorage.removeItem('logged_in');
+
+      $location.path('/login');
     }
   };
 }]);
