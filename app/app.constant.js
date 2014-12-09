15a9cdf79f3b035ca46806788f46dcddb1aedd75
Michael John Rieser <michi.rieser@gmail.com>
2014-12-09 16:31:22 +0100

    Optimizing cancel buttons. Adding endpoints for cf zhaw as a comment.

--- app/app.constant.js
+++ app/app.constant.js
@@ -1,2 +1,6 @@
 angular.module('app').constant('UAA_ENDPOINT', 'https://uaa.run.pivotal.io');
 angular.module('app').constant('API_ENDPOINT', 'https://api.run.pivotal.io');
+
+//angular.module('app').constant('UAA_ENDPOINT', 'http://uaa.cf.cclab.io');
+//angular.module('app').constant('API_ENDPOINT', 'http://api.cf.cclab.io');
+
