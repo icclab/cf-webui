09f9f2cdb207a7da80b4140812ca6cb63d8e41e2
Michael John Rieser <michi.rieser@gmail.com>
2014-12-18 17:10:07 +0100

    Adding getallheaders php function. Renaming index.html to index.php. Changing rest api endpoints to pivotal. Removing static templates.

--- request.php
+++ request.php
@@ -1,4 +1,16 @@
 <?php
+  if (!function_exists('getallheaders')) {
+    function getallheaders() {
+      $headers = '';
+      foreach ($_SERVER as $name => $value) {
+        if (substr($name, 0, 5) == 'HTTP_') {
+          $headers[str_replace(' ', '-', ucwords(strtolower(str_replace('_', ' ', substr($name, 5)))))] = $value;
+        }
+      }
+      return $headers;
+    }
+  }
+
   $method = $_SERVER['REQUEST_METHOD'];
   $url = null;
   $data = null;
