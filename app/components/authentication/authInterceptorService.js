3a04705ed4d5567b9fe998d5473627b93606a356
lidiafgarces <lidia.f.garces@gmail.com>
2015-09-21 09:39:10 +0200

    Refresh problem solved

--- app/components/authentication/authInterceptorService.js
+++ app/components/authentication/authInterceptorService.js
@@ -2,32 +2,68 @@ angular.module('app.auth').factory('authInterceptorService', ['$q', '$location',
   var authInterceptorServiceFactory = {};
 
   var _request = function(config) {
+
     config.headers = config.headers || {};
 
     var accessToken = sessionStorage.getItem('accessToken');
     var userName = sessionStorage.getItem('userName');
+
     if (config.headers.Authorization === undefined && accessToken !== null && userName !== null) {
       config.headers.Authorization = 'Bearer ' + accessToken;
       config.headers['X-Webui-Authorization'] = 'Bearer ' + accessToken;
+    }
+
+    var lastTime = sessionStorage.getItem('lastTime');
+
+    var timeOut = Date.now() - lastTime;
+    console.log('La hora ahora:');
+    console.log(Date.now());
+    console.log('La hora antes:');
+    console.log(lastTime);
+    console.log('La diferencia:');
+    console.log(timeOut);
+
+    if (timeOut < 600000){
+      console.log('Actualizo timeout');
+      sessionStorage.setItem('lastTime', Date.now());
     }
 
     return config;
   };
 
   var _responseError = function(rejection) {
+
+    var timeOut = Date.now() - sessionStorage.getItem('lastTime');
     if ($rootScope.nrOfUnauthorizedRequests === 0 && rejection.status === 401) {
       var authService = $injector.get('authService');
       var $route = $injector.get('$route');
+      var $location = $injector.get('$location');
+      var messageService = $injector.get('messageService');
+      if(timeOut > 600000){
+        console.log(timeOut);
+        console.log('Timeout');
+        //$location.path('/login');
+        authService.logOut();
+        sessionStorage.setItem('lastTime', 0);
+      }
+
       authService.refresh().then(function(response) {
-        $route.reload();
+        $route.reload(); 
       },
       function (err) {
         messageService.addMessage('danger', err.error_description);
         $log.error(err);
+        $location.path('/login');
+        authService.logOut();
+        //$location.path('/login');
+        //$route.reload();
       });
     }
 
+
     $rootScope.nrOfUnauthorizedRequests++;
+
+    $log.error(rejection);
     return $q.reject(rejection);
   };
 
