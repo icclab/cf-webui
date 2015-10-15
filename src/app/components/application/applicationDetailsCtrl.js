6ec22190cec0fa40a2909f8352ce9613a4329791
lidiafgarces <lidia.f.garces@gmail.com>
2015-10-15 17:48:29 +0200

    Porcentage truncated & JSON formated

--- src/app/components/application/applicationDetailsCtrl.js
+++ src/app/components/application/applicationDetailsCtrl.js
@@ -33,6 +33,8 @@ angular.module('app.application').controller('ApplicationDetailsCtrl', ['$rootSc
 
   $scope.scale = {};
   $scope.instances = [];
+
+  $scope.test = '{"glossary": {"title": "example glossary","GlossDiv": {"title": "S","GlossList": {"GlossEntry": {"ID": "SGML","SortAs": "SGML","GlossTerm": "Standard Generalized Markup Language","Acronym": "SGML","Abbrev": "ISO 8879:1986","GlossDef": {"para": "A meta-markup language, used to create markup languages such as DocBook.","GlossSeeAlso": ["GML", "XML"]},"GlossSee": "markup"}}}}}';
   
   // app summary
   $scope.getApplicationSummary = function() {
