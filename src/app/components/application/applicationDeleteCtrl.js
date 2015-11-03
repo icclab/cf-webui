66085b1a04aa5661b4b88b6ef6ed6e2b30998b35
lidiafgarces <lidia.f.garces@gmail.com>
2015-11-03 17:11:54 +0100

    Delete Appplication improved

--- src/app/components/application/applicationDeleteCtrl.js
+++ src/app/components/application/applicationDeleteCtrl.js
@@ -1,16 +1,35 @@
-angular.module('app.application').controller('ApplicationDeleteCtrl', ['$route', '$scope', '$modalInstance', '$log', 'applicationId', 'applicationService', 'messageService', function($route, $scope, $modalInstance, $log, applicationId, applicationService, messageService) {
+angular.module('app.application').controller('ApplicationDeleteCtrl', ['$route', '$scope', '$modalInstance', '$log', 'applicationId', 'applicationService', 'serviceBindingService', 'messageService', function($route, $scope, $modalInstance, $log, applicationId, applicationService, serviceBindingService, messageService) {
 
   $scope.applicationId = applicationId;
 
   $scope.ok = function () {
-    applicationService.deleteApplication($scope.applicationId, $route.reload()).then(function(response) {
-      messageService.addMessage('success', 'The application has been successfully deleted.');
-    }, function(err) {
-      messageService.addMessage('danger', 'The application has not been deleted.');
-      $log.error(err);
+    applicationService.getServiceBindings(applicationId).then(function(responseServiceBinding){
+      if (responseServiceBinding.data.total_results ===0){
+        applicationService.deleteApplication($scope.applicationId, $route.reload()).then(function(response) {
+          messageService.addMessage('success', 'The application has been successfully deleted.');
+          $modalInstance.close();
+        }, function(err) {
+          messageService.addMessage('danger', 'The application has not been deleted.');
+          $modalInstance.close();
+          $log.error(err);
+        });
+      }else{
+        angular.forEach(responseServiceBinding.data.resources, function(serviceBinding, i) {
+          serviceBindingService.deleteServiceBinding(serviceBinding.metadata.guid).then(function(response) {
+            applicationService.deleteApplication($scope.applicationId, $route.reload()).then(function(response) {
+              messageService.addMessage('success', 'The application has been successfully deleted.');
+              $modalInstance.close();
+            }, function(err) {
+              messageService.addMessage('danger', 'The application has not been deleted.');
+              $modalInstance.close();
+              $log.error(err);
+            }); 
+          });
+        });
+      }
+      
     });
 
-    $modalInstance.close();
   };
   
   $scope.cancel = function () {
