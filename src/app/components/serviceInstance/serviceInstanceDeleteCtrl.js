1b3adfd987f45312f9a0d10a458c037baedb0b9f
lidiafgarces <lidia.f.garces@gmail.com>
2015-11-24 17:18:21 +0100

    Delete service instance bindings before deleting service instance

--- src/app/components/serviceInstance/serviceInstanceDeleteCtrl.js
+++ src/app/components/serviceInstance/serviceInstanceDeleteCtrl.js
@@ -1,21 +1,48 @@
-angular.module('app.serviceInstance').controller('ServiceInstanceDeleteCtrl', ['$scope', '$modalInstance', '$log', 'serviceInstance', 'serviceInstanceService', 'messageService', function($scope, $modalInstance, $log, serviceInstance, serviceInstanceService, messageService) {
+angular.module('app.serviceInstance').controller('ServiceInstanceDeleteCtrl', ['$scope', '$q', '$modalInstance', '$log', 'serviceInstance', 'serviceInstanceService', 'serviceBindingService', 'messageService', function($scope, $q, $modalInstance, $log, serviceInstance, serviceInstanceService, serviceBindingService, messageService) {
 
   $scope.serviceInstance = serviceInstance;
+  $scope.serviceBindings = [];
+
+  serviceInstanceService.getServiceBindingsForServiceInstance(serviceInstance.id).then(function(response){
+    angular.forEach(response.data.resources, function(serviceBinding, key){
+      var objectServiceBinding = {
+        id: serviceBinding.metadata.guid
+      };
+      $scope.serviceBindings.push(objectServiceBinding);
+    });
+  });
 
   $scope.ok = function () {
-    serviceInstanceService.deleteServiceInstance($scope.serviceInstance.id).then(function(response) {
-      messageService.addMessage('success', 'The service instance has been successfully deleted.');
+    angular.forEach($scope.serviceBindings, function(serviceBinding, key){
+      console.log(serviceBinding.id);
+    });
 
-      $modalInstance.close($scope.serviceInstance);
+    $scope.deleteServiceBindings($scope.serviceBindingId).then(function(response) {
+      serviceInstanceService.deleteServiceInstance($scope.serviceInstance.id).then(function(response) {
+        messageService.addMessage('success', 'The service instance has been successfully deleted.');
+
+        $modalInstance.close($scope.serviceInstance);
+      }, function(err) {
+        messageService.addMessage('danger', 'The service instance has not been deleted.');
+        $log.error(err);
+
+        $modalInstance.dismiss('cancel');
+      });
     }, function(err) {
-      messageService.addMessage('danger', 'The service instance has not been deleted.');
+      messageService.addMessage('danger', 'The service has not been unbound.');
       $log.error(err);
-
-      $modalInstance.dismiss('cancel');
     });
   };
   
   $scope.cancel = function () {
     $modalInstance.dismiss('cancel');
+  };
+
+  $scope.deleteServiceBindings = function () {
+    var promises = [];
+    angular.forEach($scope.serviceBindings, function(serviceBinding, key){
+      promises.push(serviceBindingService.deleteServiceBinding(serviceBinding.id));
+    });
+    return $q.all(promises);
   };
 }]);
